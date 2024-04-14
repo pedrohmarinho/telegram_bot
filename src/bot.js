f61fd94a7bf93e2b6a22d2f40d34cbe4a98814da
pedromarinho262@gmail.com <pedromarinho262@gmail.com>
2024-04-14 15:00:02 -0300

    Adiciona API do bot do Telegram e do Sushido

--- src/bot.js
+++ src/bot.js
@@ -1,29 +1,70 @@
+// Telegram bot
 const TelegramBot = require('node-telegram-bot-api');
 
-// replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.BOT_TOKEN;
 
-// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
+bot.onText(/\/classificar/, (message) => {
+  const chatId = message.chat.id;
+  bot.sendMessage(chatId, "Envie uma imagem para eu classificá-la!");
 
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
+  bot.on("photo", (photoMessage) => handlePhoto(photoMessage));
 });
 
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
+// Sashido
+const TeachableMachine = require("@sashido/teachablemachine-node");
 
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+const model = new TeachableMachine({
+  modelUrl: "https://teachablemachine.withgoogle.com/models/r6BBk-hiN/"
 });
+
+model.classify({
+  imageUrl: "https://media-blog.sashido.io/content/images/2020/09/SashiDo_Dog.jpg",
+}).then((predictions) => {
+  console.log("Predictions:", predictions);
+}).catch((e) => {
+  console.log("ERROR", e);
+}); 
+
+// Functions handlePhoto e classifyImage
+function handlePhoto(message) {
+    const chatId = message.chat.id;
+    const photoId = message.photo ? message.photo[message.photo.length - 1].file_id : null;
+  
+    if (!photoId) {
+      return bot.sendMessage(chatId, "Nenhuma foto encontrada para classificação.");
+    }
+  
+    bot.downloadFile(photoId, "./src/image").then(async (imagePath) => {
+      try {
+        const fileName = imagePath.split("/").pop();
+        const serverAddress = server.getAddress();
+  
+        const imageUrl = `${serverAddress}/image/${fileName}`;
+        bot.sendMessage(chatId, "Classificando imagem...");
+  
+        const predictionResult = await classifyImage(imageUrl);
+        server.removeFile(fileName);
+  
+        bot.sendMessage(chatId, `Na imagem contém um ${predictionResult.class}`);
+      } catch (error) {
+        console.log("error", error);
+        bot.sendMessage(chatId, `Erro ao classificar a imagem`);
+      }
+    }).catch((error) => {
+      console.log("error", error);
+      bot.sendMessage(chatId, "Erro ao baixar a imagem para classificação.");
+    });
+  }
+  
+  async function classifyImage(imageUrl) {
+    try {
+      const predictions = await model.classify({ imageUrl });
+      const highConfidencePrediction = predictions.find(prediction => prediction.score > 0.5);
+      return highConfidencePrediction;
+    } catch (error) {
+      throw new Error("Erro ao classificar a imagem");
+    }
+  }
+  
